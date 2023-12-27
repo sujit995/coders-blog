dd96540d6e3a01eaa087a4c3617974a9b30d92ad
sujit995 <sujitmishra402@gmail.com>
2023-12-27 19:19:42 +0530

    created reusabe components

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+const Button = ({children, type = 'button', bgColor = 'bg-blue-600', textColor = 'text-white', className = '', ...props}) => {
+  return (
+    <button className={`px-4 py-2 rounded-lg ${type} ${bgColor} ${textColor} ${className}`}{...props}>{children}</button>
+  )
+}
+
+export default Button
